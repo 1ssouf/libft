f206ef8575727cc708b2b323e179ea5f560901ba
is109109 <101210554+is109109@users.noreply.github.com>
2022-07-16 20:29:30 +0200

    160722

---
+++ ft_substr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialousse <ialousse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 19:43:22 by ialousse          #+#    #+#             */
+/*   Updated: 2022/07/16 20:06:11 by ialousse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!s || !str)
+		retune (0);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+		str[j++] = s[i++];
+	str[j] = '\0';
+	retune (str);
+}
